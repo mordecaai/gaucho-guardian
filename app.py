f789d2aa23690dbfd117815c6f2c4d99e3edf8ea
Landon Gemmill <landon@gemmill.net>
2026-01-10 12:17:12 -0800

    add launch file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
